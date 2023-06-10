99ea023f5594913a2cb9af7ddbe2918780d8a097
Yaroslav <yaroslavcher@gmail.com>
2023-06-10 11:37:11 +0300

    fixed tests

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,72 +1,7 @@
-/*
-
-import io.appium.java_client.AppiumDriver;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import ru.netology.MainScreen;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
-import static io.appium.java_client.remote.IOSMobileCapabilityType.*;
-import static io.appium.java_client.remote.MobileCapabilityType.*;
-import static org.junit.jupiter.api.TestInstance.Lifecycle;
-import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
-
-@TestInstance(Lifecycle.PER_CLASS)
-public class SampleTest {
-
-    private AppiumDriver driver;
-
-    @BeforeAll
-    public void createDriver() throws MalformedURLException {
-        String platform = System.getProperty("platform");
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        if ("android".equals(platform)) {
-            desiredCapabilities.setCapability(PLATFORM_NAME, "android");
-            desiredCapabilities.setCapability(DEVICE_NAME, "any name");
-            desiredCapabilities.setCapability(APP_PACKAGE, "com.google.android.calculator");
-            desiredCapabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator");
-        } else if ("ios".equals(platform)) {
-            desiredCapabilities.setCapability(PLATFORM_NAME, "iOS");
-            desiredCapabilities.setCapability(DEVICE_NAME, "iPhone 11");
-            desiredCapabilities.setCapability(BUNDLE_ID, "com.shubham-iosdev.Calculator-UI");
-            desiredCapabilities.setCapability(AUTOMATION_NAME, "XCUITest");
-        } else {
-            throw new IllegalArgumentException(String.format("Platform %s no supported", platform));
-        }
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-    }
-
-    @Test
-    public void test() {
-        MainScreen mainScreen = new MainScreen(driver);
-        mainScreen.digitTwoButton.click();
-        mainScreen.plusSignButton.click();
-        mainScreen.digitTwoButton.click();
-        mainScreen.equalsSignButton.click();
-        Assertions.assertEquals("4", mainScreen.resultField.getText());
-    }
-
-    @AfterAll
-    public void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-}*/
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-/*import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;*/
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -93,23 +28,22 @@ public class SampleTest {
 
     @Test
     public void inputEmptyStringTest() {
-        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
-        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
-        el2.click();
-        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
-        Assertions.assertEquals(el3.getText(), el1.getText());
+        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
+        el3.click();
+        Assertions.assertNotEquals (el1.getText(), el2.getText());
     }
 
     @Test
     public void openTextInNewActivityTest() {
         MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
         el1.click();
-        el1.sendKeys("test");
-        //el1.click();
+        el1.sendKeys("123");
         MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout");
-        Assertions.assertEquals("true", el3.isDisplayed());
+        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/text");
+        Assertions.assertEquals("123", el3.getText());
     }
 
     @AfterEach
